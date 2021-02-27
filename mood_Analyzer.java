4203d362db9b6cd9900751a05f85d599f6b2e2df
pravin bagul <bagul.pravin2@gmail.com>
2021-02-27 15:14:57 +0530

    Ability_To_Check

--- mood_Analyzer.java
+++ mood_Analyzer.java
@@ -1,47 +1,30 @@
 package com.mood.analyzer;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+public class MoodAnalyzer {
 
-public class MoodAnalyzerTest {
+	public String message;
 
-	@Test
-	public void givenMessgae_WhenSad_ShouldReturn_Sad() {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Sad Mood");
-		String mood = null;
-		try {
-			mood = moodAnalyser.analyseMood();
-			Assert.assertEquals("SAD", mood);
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
-	}
-
-	@Test
-	public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any Mood");
-		String mood = null;
-		try {
-			mood = moodAnalyzer.analyseMood();
-			Assert.assertEquals("HAPPY", mood);
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
+	public MoodAnalyzer() {
 
 	}
 
-	@Test
-	public void givenNullMood_ShouldReturn_Happy() {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
-		String mood = null;
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
 		try {
-			mood = moodAnalyser.analyseMood();
-			Assert.assertEquals("HAPPY", mood);
-		} catch (Exception e) {
-			e.printStackTrace();
+			if (message.contains("sad")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException e) {
+			return "HAPPY";
 		}
+	}
+
+	public static void main(String Args[]) {
+		System.out.println("Welcome to mood Analyser");
 	}
 }
